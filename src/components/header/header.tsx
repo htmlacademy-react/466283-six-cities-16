11fb6c105da4d5cc4e46c96f4ba2ec665b2cff7e
Vladislav Vlasenko <you@example.comvlad-vlasenko@mail.ru>
2024-07-16 23:00:09 +0300

    Меняет логику

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,13 +1,15 @@
 import HeaderLogo from '../header-logo/header-logo';
 import HeaderAuth from '../header-auth/header-auth';
-
+import { useLocation } from 'react-router-dom';
 function Header(): JSX.Element {
+  const location = useLocation();
+
   return (
     <header className="header">
       <div className="container">
         <div className="header__wrapper">
           <HeaderLogo />
-          <HeaderAuth />
+          {location.pathname !== '/login' && <HeaderAuth />}
         </div>
       </div>
     </header>
--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -1,9 +1,9 @@
-import HeaderLogin from '../../components/header-login/header-login';
+import Header from '../../components/header/header';
 
 function Login(): JSX.Element {
   return (
     <div className="page page--gray page--login">
-      <HeaderLogin />
+      <Header />
       <main className="page__main page__main--login">
         <div className="page__login-container container">
           <section className="login">
