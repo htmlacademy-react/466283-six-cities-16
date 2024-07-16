7828d965f07ad62a0fb612327bfddb39d3036a6c
Vladislav Vlasenko <you@example.comvlad-vlasenko@mail.ru>
2024-07-16 20:55:25 +0300

    добавляет стили

--- src/pages/not-found/not-found.tsx
+++ src/pages/not-found/not-found.tsx
@@ -1,13 +1,16 @@
 import { Link } from 'react-router-dom';
 import { AppRoute } from '../../const';
+import './not-found.css';
 function NotFound(): JSX.Element {
   return (
     <div className="page-not-found">
-      <div className="page-not-found__title">Страница не найдена</div>
+      <div className="page-not-found__wrapper">
+        <div className="page-not-found__title">Страница не найдена</div>
+        <Link to={AppRoute.Root} className="page-not-found__link">
+          На главную
+        </Link>
+      </div>
       <div className="page-not-found__error">404</div>
-      <Link to={AppRoute.Root} className="page-not-found__link">
-        На главную
-      </Link>
     </div>
   );
 }
